5a7208dba7d35ef82e7e6c36d2fb586422738bf5
Jan Kovářík <jan.kovarik@argo22.com>
2021-06-05 19:16:42 +0200

    PageHead component

---
+++ src/components/shared/PageHead/PageHead.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+// TODO: title, description, favicon
+
+export function PageHead() {
+  return (
+    <Head>
+      <title>Virtual Tours</title>
+
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
+import { PageHead } from "components/shared";
 import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <PageHead />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
