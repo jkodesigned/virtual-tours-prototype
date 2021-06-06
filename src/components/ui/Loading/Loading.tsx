cf0b52adf6a8e84e816c9bac1a740225129eb1ed
Jan Kovářík <jan.kovarik@argo22.com>
2021-06-06 10:07:27 +0200

    Loading reimplementation with css

--- src/components/ui/Loading/Loading.tsx
+++ src/components/ui/Loading/Loading.tsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import { useInterval } from "react-use";
-import { t } from "@lingui/macro";
-import Image from "next/image";
+import clsx from "clsx";
+import s from "components/ui/Loading/Loading.module.css";
 
-const DELAY = 400; // ms
+const DELAY = 500; // ms
 
 const frames = [
-  "/images/loading/frame-0.png",
-  "/images/loading/frame-1.png",
-  "/images/loading/frame-2.png",
-  "/images/loading/frame-3.png",
-  "/images/loading/frame-4.png",
-  "/images/loading/frame-5.png",
-  "/images/loading/frame-6.png",
+  [clsx(s.topLeft), clsx(s.bottomRight), clsx(s.topRight), clsx(s.bottomLeft)],
+  [clsx(s.topRight, s.green), clsx(s.bottomLeft, s.green), clsx(s.topRight), clsx(s.bottomLeft)],
+  [clsx(s.topRight), clsx(s.bottomLeft), clsx(s.topLeft), clsx(s.bottomRight)],
+  [clsx(s.bottomRight, s.green), clsx(s.topLeft, s.green), clsx(s.topLeft), clsx(s.bottomRight)],
+  [clsx(s.bottomRight), clsx(s.topLeft), clsx(s.bottomLeft), clsx(s.topRight)],
+  [clsx(s.topRight), clsx(s.bottomLeft), clsx(s.bottomRight), clsx(s.topLeft)],
+  [clsx(s.topRight, s.green), clsx(s.bottomLeft, s.green), clsx(s.bottomLeft), clsx(s.topRight)],
 ];
 
 const lastFrame = frames.length - 1;
@@ -22,5 +22,12 @@ export function Loading() {
 
   useInterval(() => setFrame((frame) => (frame === lastFrame ? 0 : frame + 1)), DELAY);
 
-  return <Image src={frames[frame]} alt={t`Loading`} width={120} height={120} />;
+  return (
+    <div className={s.container}>
+      <div className={clsx(s.letter, frames[frame][0])}>V</div>
+      <div className={clsx(s.letter, frames[frame][1])}>H</div>
+      <div className={clsx(s.box, frames[frame][2])} />
+      <div className={clsx(s.box, frames[frame][3])} />
+    </div>
+  );
 }
